a9b335340f8edd9698b6c9c74ed7d308e336e128
ankuransaha <121350252+ankuransaha@users.noreply.github.com>
2025-08-23 05:56:46 +0530

    ank_library.py

---
+++ ank_library.py
@@ -0,0 +1,10 @@
+def add_numbers(a,b):
+  return a+b
+def subtract_numbers(a,b):
+  return a-b
+def multiply_numbers(a,b):
+  return a*b
+def divide_numbers(a,b)
+  if b==0
+    return "Error: Cannot divide by Zero"
+  return a/b
